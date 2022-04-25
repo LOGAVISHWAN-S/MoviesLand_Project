dd1b7421cb1d580e998c93bec4fc1c9457bb9caa
LOGAVISHWAN-S <vishwan219@gmail.com>
2022-04-25 22:05:30 +0530

    completed half way

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+import '../src/App.css';
+import SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=c032e2d7';
+ const App = () => {
+     const searchMovies = async (title) => {
+         const response = await fetch(`${API_URL}&s=${title}`);
+         const data = await response.json();
+
+         console.log(data.Search);
+     }
+
+     useEffect(() => {
+        searchMovies('spiderman');
+     }, []);
+
+     return (
+         <div className="app">
+             <h1>MovieLand</h1>
+
+             <div className="search">
+                 <input
+                   placeholder="Search for movies"
+                   value="Superman"
+                   onChange={() => {}}
+                />
+                <img
+                src={SearchIcon}
+                alt="search"
+                onClick={() => {}}
+                />
+             </div>
+
+             <div className="container">
+                 <div className='movie'>
+                     <div>
+                         <p>
+                             {Year}
+                         </p>
+                     </div>
+
+                     <div>
+                         <img src={Poster !== 'N/A' ? Poster : 'http://via.placeholder.com/400'} alt={Title}/>
+                     </div>
+
+                     <div>
+                         <span>{Type} </span>
+                         <h3>{Title} </h3>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     );
+ }
+
+ export default App;
