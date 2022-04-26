43db2a139485edb8be479a08f7c44f16554e24c1
LOGAVISHWAN-S <vishwan219@gmail.com>
2022-04-26 20:52:06 +0530

    Finished the movieLand application

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+
+import MovieCard from './MovieCard';
 
 
-import '../src/App.css';
+import './App.css';
 import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=c032e2d7';
  const App = () => {
+   const [movies, setMovies] = useState([]);
+   const [searchTerm, setSearchTerm] = useState('');
+
+
      const searchMovies = async (title) => {
          const response = await fetch(`${API_URL}&s=${title}`);
          const data = await response.json();
 
-         console.log(data.Search);
+         setMovies(data.Search);
      }
 
      useEffect(() => {
@@ -24,34 +30,32 @@ const API_URL = 'http://www.omdbapi.com?apikey=c032e2d7';
              <div className="search">
                  <input
                    placeholder="Search for movies"
-                   value="Superman"
-                   onChange={() => {}}
+                   value={searchTerm}
+                   onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img
                 src={SearchIcon}
                 alt="search"
-                onClick={() => {}}
+                onClick={() => searchMovies(searchTerm)}
                 />
              </div>
 
-             <div className="container">
-                 <div className='movie'>
-                     <div>
-                         <p>
-                             {Year}
-                         </p>
-                     </div>
+             {
+                 movies.length > 0
+                 ? (
+                    <div className="container">
+                      {movies.map((movie) => (
+                          <MovieCard movie={movie} />
+                      ))}
+                    </div>
+                 ) : (
+                     <div className="empty">
+                         <h2>No movies found</h2>
+                     </div>    
+                 )
+             }
 
-                     <div>
-                         <img src={Poster !== 'N/A' ? Poster : 'http://via.placeholder.com/400'} alt={Title}/>
-                     </div>
-
-                     <div>
-                         <span>{Type} </span>
-                         <h3>{Title} </h3>
-                     </div>
-                 </div>
-             </div>
+             
          </div>
      );
  }
